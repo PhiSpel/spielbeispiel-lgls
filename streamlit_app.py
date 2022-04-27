2cde2370275b5747566e3f3a78a707591b9e9037
PhiSpel <philipp.spelten@web.de>
2022-04-27 17:27:01 +0200

    trying out count_sessions from https://discuss.streamlit.io/t/streamlit-app-view-and-statistics/8017/2

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,8 @@ import matplotlib.colors as mcol
 import matplotlib.cm as cm
 
 import streamlit as st
+from state import count_sessions
+count_sessions()
 
 ###############################################################################
 # CALCULATIONS
